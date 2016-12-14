0286c4ec723bb260721b7f8b79a3c93242a8a96f
abc@cin.ufpe.br <abc@cin.ufpe.br>
2016-12-14 17:01:17 -0300

    fdb

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,8 @@
 
 module.exports = function(app) {
   	var controller = app.controllers.convoy;
-  	app.post('/register', controller.register);
+  	app.get('/registered', controller.registered);
+	app.post('/register', controller.register);
   	app.post('/updatePosition', controller.updatePosition);
   	app.post('/emergencyStop', controller.emergencyStop);
 };
